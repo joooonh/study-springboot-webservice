eaf214bada782b81cbef47f7feecf59530661adb
junhee <joooonh12@gmail.com>
2023-08-18 17:35:03 +0900

    게시글 수정, 삭제

--- src/main/resources/static/js/app/index.js
+++ src/main/resources/static/js/app/index.js
@@ -11,7 +11,6 @@ var main = {
         });
 
         $('#btn-delete').on('click', function () {
-            console.log('삭제');
             _this.delete();
         });
     },
@@ -62,6 +61,17 @@ var main = {
     delete : function () {
         var id = $('#id').val();
 
+        $.ajax( {
+            type : 'DELETE',
+            url : '/api/v1/posts/' + id,
+            dataType : 'json',
+            contentType : 'application/json; charset=utf-8',
+        }).done(function() {
+            alert('글이 삭제되었습니다.');
+            window.location.href = '/';
+        }).fail(function(error) {
+            alert(JSON.stringify(error));
+        })
     }
 };
 
