8d3e658e1379cb9b0f21310e7befb83734eebe18
junhee <joooonh12@gmail.com>
2023-08-18 22:01:34 +0900

    스프링 시큐리티 설정

---
+++ src/main/java/com/example/studyspringbootwebservice/config/auth/CustomOAuth2UserService.java
@@ -0,0 +1,62 @@
+package com.example.studyspringbootwebservice.config.auth;
+
+import com.example.studyspringbootwebservice.config.auth.dto.OAuthAttributes;
+import com.example.studyspringbootwebservice.config.auth.dto.SessionUser;
+import com.example.studyspringbootwebservice.domain.user.User;
+import com.example.studyspringbootwebservice.domain.user.UserRepository;
+import jakarta.servlet.http.HttpSession;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
+import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
+import org.springframework.security.oauth2.core.user.DefaultOAuth2User;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.stereotype.Service;
+
+import java.util.Collections;
+
+@RequiredArgsConstructor
+@Service
+public class CustomOAuth2UserService implements OAuth2UserService<OAuth2UserRequest, OAuth2User> {
+
+    private final UserRepository userRepository;
+    private final HttpSession httpSession;
+
+    @Override
+    public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
+
+        // 기본적으로 제공되는 DefaultOAuth2UserSevice 사용 : OAuth 2.0 공급자로부터 받은 액세스 토큰을 사용하여 사용자 정보를 가져옴
+        OAuth2UserService<OAuth2UserRequest, OAuth2User> delegate = new DefaultOAuth2UserService();
+        // 사용자 정보
+        OAuth2User oAuth2User = delegate.loadUser(userRequest);
+
+        // 현재 로그인 진행 중인 OAuth 2.0 서비스를 구분하는 코드
+        String registrationId = userRequest.getClientRegistration().getRegistrationId();
+
+        // OAuth 2.0 로그인 진행 시 키가 되는 필드값. 구글은 기본적으로 코드 지원(sub)
+        String userNameAttributeName = userRequest.getClientRegistration().getProviderDetails().getUserInfoEndpoint().getUserNameAttributeName();
+
+        // OAuth2UserService를 통해 가져온 OAuth2User의 attribute를 담을 클래스
+        OAuthAttributes attributes = OAuthAttributes.of(registrationId, userNameAttributeName, oAuth2User.getAttributes());
+
+        // 조회한 OAuth2User가 있을 경우 update, 없을 경우 save
+        User user = saveOrUpdate(attributes);
+
+        // 세션에 사용자 정보를 저장하기 위한 Dto 클래스
+        httpSession.setAttribute("user", new SessionUser(user));
+
+        return new DefaultOAuth2User(
+                Collections.singleton(new SimpleGrantedAuthority(user.getRoleKey())),
+                attributes.getAttributes(),
+                attributes.getNameAttributeKey());
+    }
+
+    private User saveOrUpdate(OAuthAttributes attributes) {
+        User user = userRepository.findByEmail(attributes.getEmail())
+                                .map(entity -> entity.update(attributes.getName(), attributes.getPicture()))
+                                .orElse(attributes.toEntity());
+        return userRepository.save(user);
+    }
+}
---
+++ src/main/java/com/example/studyspringbootwebservice/config/auth/SecurityConfig.java
@@ -0,0 +1,39 @@
+package com.example.studyspringbootwebservice.config.auth;
+
+import com.example.studyspringbootwebservice.domain.user.Role;
+import jakarta.servlet.DispatcherType;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@RequiredArgsConstructor
+@EnableWebSecurity
+@Configuration
+public class SecurityConfig {
+
+    private final CustomOAuth2UserService customOAuth2UserService;
+
+    @Bean
+    protected SecurityFilterChain config(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()                           // 토큰 검증 비활성화
+                .headers().frameOptions().disable()         // h2-console 화면을 사용하기 위해 해당 옵션들 disable
+                .and()
+                    .authorizeHttpRequests(                 // http 요청에 대한 인증, 권한 설정 수행
+                            request -> request
+                                    .dispatcherTypeMatchers(DispatcherType.FORWARD).permitAll()     // Forwad 요청에 대해 모든 사용자의 접근 허용
+                                    .requestMatchers("/", "/css/**", "/images/**", "/js/**", "/h2-console/**").permitAll()
+                                    .requestMatchers("/api/v1/**").hasRole(Role.USER.name())      // 해당 경로에 대해서는 USER 권한을 가진 사용자만 접근 허용
+                                    .anyRequest().authenticated()                                   // 이외의 모든 요청은 인증된 사용자만 접근 허용
+                    )
+                    .logout().logoutSuccessUrl("/")
+                .and()
+                    .oauth2Login()                              // oauth2.0 로그인 설정
+                        .userInfoEndpoint()                     // 로그인 성공한 사용자 정보를 가져올 때의 설정
+                        .userService(customOAuth2UserService);  // 로그인 성공 시 수행할 service 등록
+        return http.build();
+    }
+}
