9c46fabf5cf0bb314318a4aa97af09c9c17e44e3
junhee <joooonh12@gmail.com>
2023-08-20 17:04:50 +0900

    로그인 어노테이션 적용

--- src/main/java/com/example/studyspringbootwebservice/web/IndexController.java
+++ src/main/java/com/example/studyspringbootwebservice/web/IndexController.java
@@ -1,5 +1,6 @@
 package com.example.studyspringbootwebservice.web;
 
+import com.example.studyspringbootwebservice.config.auth.LoginUser;
 import com.example.studyspringbootwebservice.config.auth.dto.SessionUser;
 import com.example.studyspringbootwebservice.service.posts.PostsService;
 import com.example.studyspringbootwebservice.web.dto.PostsResponseDto;
@@ -18,10 +19,9 @@ public class IndexController {
     private final HttpSession httpSession;
 
     @GetMapping("/")
-    public String index(Model model) {
+    public String index(Model model, @LoginUser SessionUser user) {
         model.addAttribute("posts", postsService.findAllDesc());
 
-        SessionUser user = (SessionUser) httpSession.getAttribute("user");
         if (user != null) {
             model.addAttribute("userName", user.getName());
         }
